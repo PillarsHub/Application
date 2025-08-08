c592933886186c1158026ca6efc5289bd5ece595
Leonard Cooke <leonardlcooke@hotmail.com>
2025-08-07 18:53:35 -0600

    Updated placement suite disclaimer.

--- src/pages/customers/treeComponents/placementSuite.js
+++ src/pages/customers/treeComponents/placementSuite.js
@@ -89,8 +89,11 @@ const PlacementSuite = ({ nodeId, treeId, shows, onHide, handlePlaceNode }) => {
     </div>
     <div className="card-footer">
       <div className="alert alert-warning" role="alert">
-        <h4 className="alert-title">Commissions Disclaimer</h4>
-        <div className="text-muted">Any Sponsor Tree-based commission paid on the Brand Partner being placed for the open commission month will be removed from your monthly commission and paid to the new Sponsor.</div>
+        <h4 className="alert-title">Placement Suite Disclaimer</h4>
+        <div className="text-muted">
+          Before using the placement suite to move a personally enrolled and sponsored Brand Partner off your first level, please consider the personally enrolled and sponsored Level 1 requirements for Rank advancements. 
+          Please review the <a href="https://7ity.me/rewards-plan" target="_blank" rel="noreferrer" >Sevinity Rewards Plan</a> or contact your upline for further guidance. 
+        </div>
       </div>
     </div>
   </OffCanvas>
